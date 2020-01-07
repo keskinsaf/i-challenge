9557241e5f8a677ddf8b39f6ddcc0c5bd1cc2f5b
Safa Keskin <keskinsaf@gmail.com>
2020-01-07 21:34:26 +0300

    feat(app.py): add parameter validation & return image to client

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, request, abort
-
-from util import create_hex_hash
+from flask import Flask, request, abort, send_file
+from io import BytesIO
+from util import create_hex_hash, generate_image_from_hash
 
 app = Flask(__name__)
+
+MAX_SIZE = 500
+ALLOWED_ROWS = {3: True, 5: True, 7: True}
 
 
 @app.route('/')
@@ -14,11 +17,32 @@ def hello_world():
 def identicon_generator(username: str):
     if not username.endswith(".png"):
         abort(400, "A png image should be requested. Read docs.")
-    size = request.args.get('size', default=300, type=int)
-    row = request.args.get('row', default=5, type=int)
+    size = abs(request.args.get('size', default=300, type=int))
+    row = abs(request.args.get('row', default=5, type=int))
+
+    if row in ALLOWED_ROWS:
+        print("Row: {} is allowed.".format(row))
+    else:
+        print("Row: {} is not allowed. Will be set to 5.".format(row))
+        row = 5
+
+    if size <= MAX_SIZE:
+        print("Size: {} is allowed.".format(size))
+    else:
+        print("Size: {} is not allowed. Will be set to {}.".format(size, MAX_SIZE))
+        size = MAX_SIZE
+
+    row = row if row in ALLOWED_ROWS else 5
+
     print("username: {}\t\t size: {}\t\t row: {}".format(username, size, row))
-    # meta = {"username": username, "size": size, "row": row}
-    return create_hex_hash(username)
+    hex_hash = create_hex_hash(username)
+    identicon = generate_image_from_hash(hex_hash, size, row)
+    # identicon.save("images/l_" + username, "png") # saving locally
+
+    bin_img = BytesIO()
+    identicon.save(bin_img, 'png')
+    bin_img.seek(0)
+    return send_file(bin_img, mimetype='image/png')
 
 
 @app.errorhandler(400)
