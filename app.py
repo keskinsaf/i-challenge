a9c996627e612d38fcaf36eacd79adc1d1864976
Safa Keskin <keskinsaf@gmail.com>
2020-01-07 17:03:13 +0300

    feat(app.py): add expected request handling without identicon generating functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,16 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/<username>', methods=['GET'])
+def identicon_generator(username):
+    print(username)
+    size = request.args.get('size', default=300, type=int)
+    row = request.args.get('row', default=5, type=int)
+    print("username: {}\t\t size: {}\t\t row: {}".format(username, size, row))
+    meta = {"username": username, "size": size, "row": row}
+    return meta
 
 
 if __name__ == '__main__':
