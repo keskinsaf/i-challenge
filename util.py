103f3d29f5dfc8dd6bb8dcca991e9356858c96cd
Safa Keskin <keskinsaf@gmail.com>
2020-01-09 02:36:00 +0300

    feat(util.py): make result images similar to GitHub's identicon     with their margins and vertically symmetric appearance fix(util.py): fix piece taking part of generate_image_from_hash function

--- util.py
+++ util.py
@@ -1,4 +1,4 @@
-import hashlib
+import hashlib, math
 from PIL import Image
 
 
@@ -23,6 +23,15 @@ def get_color_from_hash(hex_hash: str):
     return tuple([clr % 256 for clr in ls])
 
 
+def put_pixel(image: Image, top_left_coord: (int, int), color: (int, int, int), scale: int = 3):
+    y, x = top_left_coord
+    # for each row
+    for i in range(scale):
+        # for each col
+        for j in range(scale):
+            image.putpixel((x + j, y + i), color)
+
+
 # generate image from hash
 def generate_image_from_hash(hex_hash: str, size: int, row: int):
     print("An {}x{} sized image with {} rows will be generated.".format(size, size, row))
@@ -30,13 +39,21 @@ def generate_image_from_hash(hex_hash: str, size: int, row: int):
 
     piece_capacity = len(hex_hash) // (row ** 2)
 
+    img_res = 2 + row * 3
     # create Image
-    image = Image.new(mode="RGB", size=(row, row), color=(128, 128, 128))
+    image = Image.new(mode="RGB", size=(img_res, img_res), color=(255, 255, 255))
 
-    for row_ctr in range(row):
-        for col_ctr in range(row):
-            piece_idx = row_ctr * piece_capacity + col_ctr
+    for row_idx in range(1, 1 + (row * 3), 3):
+        row_ctr = (row_idx - 1) // 3
+        for col_idx in range(1, math.ceil(1 + (row * 3)/2), 3):
+            col_ctr = (col_idx - 1) // 3
+            piece_idx = piece_capacity * (row_ctr * row + col_ctr)
             piece = hex_hash[piece_idx:piece_idx + piece_capacity]
             if sum(map(ord, piece)) % 2 == 1:
-                image.putpixel((row_ctr, col_ctr), color)
+                put_pixel(image, (row_idx, col_idx), color)
+
+    for row_idx in range(1, 1 + (row * 3)):
+        for col_idx in range(math.ceil(1 + (row * 3)/2), img_res):
+            image.putpixel((col_idx, row_idx), image.getpixel((img_res - 1 - col_idx, row_idx)))
+
     return image.resize(size=(size, size), resample=Image.NEAREST)
